0dc0dfc9d3c36d1173c9a2701bac08267d3dc3ff
primojr <gersonprimo@gmail.com>
2025-01-13 20:45:06 -0300

    resolução da lista 1 até a questão 7

---
+++ r/lista1.R
@@ -0,0 +1,63 @@
+# Lista de exercicio 1
+
+# Q1 - Gerador RANDU
+# Criar uma funcão para Gerador RANDU em R
+randu <- function(n, seed = 1, modulo = 2^31) {
+  x <- seed
+  u <- numeric(n) 
+  for (i in 1:n) {
+    x <- (65539 * x) %% modulo
+    u[i] <- x / modulo
+  }
+  return(u)
+}
+
+randu(1000) |> hist()
+
+# Q2 - Gerador Aleatorio - sequencia
+gerador_seq <- function(n) {
+  x <- numeric(n)  
+  x[1] <- 23
+  x[2] <- 66
+  
+  for (i in 3:n) {
+    x[i] <- (3 * x[i - 1] + 5 * x[i - 2]) %% 100
+  }
+  
+  u <- x / 100
+  
+  list(xn = x, un = u)
+}
+
+n <- 14
+resultado <- gerador_seq(n)
+resultado$xn
+
+# Q3 - Função de probabilidade
+F_x <- function() {
+  repeat {
+    u <- runif(1)
+    if (u <= 1/3) {
+      return(1)
+    } else {
+      return(2)
+    }
+  }
+}
+
+# a) n = 100  
+n = 100
+resultado <- purrr::map_dbl(1:n, ~ F_x())
+resultado |> janitor::tabyl()
+
+# b) n = 1000
+n = 1000
+resultado <- purrr::map_dbl(1:n, ~ F_x())
+resultado |> janitor::tabyl()
+
+# c) n = 10000
+n = 10000
+resultado <- purrr::map_dbl(1:n, ~ F_x())
+resultado |> janitor::tabyl()
+
+
---
+++ r/metodo-rejeicao.R
@@ -0,0 +1,55 @@
+#
+# Metodo da rejeição - Geral
+#
+metodo_rejeicao <- function() {
+  repeat {
+    x <- runif(n = 1, min = -1, max = 1)
+    u <- runif(n = 1, min = 0, max = 1)
+
+    if (u <= 1 - x^2) {
+      return(list(x = x, contagem = "Aceito"))
+    } else {
+      return(list(x = NA, contagem = "Rejeito"))
+    }
+  }
+}
+
+n = 10000
+metodo_rejeicao()
+amostra <- purrr::map_df(1:n, ~metodo_rejeicao())  
+
+amostra$contagem |>
+  table() |>
+  prop.table()
+
+amostra$x |> hist(probability = TRUE, main = paste("N = ", n))
+curve(3 / 4 * (1 - x^2), from = -1, to = 1, col = "red", lwd = 1, add = TRUE)
+
+#
+# Metodo da rejeição - “squeezing” 
+#
+metodo_rejeicao_squeezing <- function() {
+  repeat {
+    x <- runif(n = 1, min = -1, max = 1)
+    u <- runif(n = 1, min = 0, max = 1)
+
+    if (u <= 1 - abs(x)) {
+      return(list(x = x, contagem = "Aceito Cond.1"))
+    } else if (u <= 1 - x^2) {
+      return(list(x = x, contagem = "Aceito Cond.2"))
+    } else {
+      return(list(x = NA, contagem = "Rejeito"))
+    }
+  }
+}
+
+n = 100000
+metodo_rejeicao()
+amostra <- purrr::map_df(1:n, ~metodo_rejeicao_squeezing())  
+
+amostra$contagem |> table() |> prop.table()
+
+amostra$x |> hist(probability = TRUE)
+curve(3/4*(1-x^2), from = -1, to = 1, col = "red", lwd = 1, add = TRUE)
+
+
